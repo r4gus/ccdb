6104c3a8fade46f3e9ef56c0d9d3d1d4afd1717c
David P. Sugar <david@thesugar.de>
2024-07-01 22:26:03 +0200

    back to the roots

--- src/root.zig
+++ src/root.zig
@@ -7,6 +7,10 @@ const chacha = @import("chacha.zig");
 const XChaCha20IETF = chacha.XChaCha20IETF;
 
 // +------------------------------------------------+
+// | Blocks                                         |
+// +------------------------------------------------+
+
+// +------------------------------------------------+
 // | Options                                        |
 // +------------------------------------------------+
 
@@ -22,7 +26,7 @@ pub const OptionType = enum(u16) {
 
     pub fn fromRaw(i: [2]u8) !@This() {
         const i_ = @as(u16, @intCast(i[1])) << 8 | @as(u16, @intCast(i[0]));
-        return switch (i) {
+        return switch (i_) {
             0 => .endofopt,
             1 => .comment,
             2988 => .custom_utf8,
@@ -43,40 +47,40 @@ pub const Option = struct {
     }
 };
 
-pub const OptionIterator = struct {
-    data: []const u8,
-    i: usize = 0,
-
-    pub fn next(self: *@This()) ?Option {
-        if (self.i >= self.data.len) return null;
-        if (self.data[self.i..].len < 4) {
-            self.i = self.data.len;
-            return null;
-        }
-        const t = OptionType.fromRaw(self.data[0..2].*) catch {
-            self.i = self.data.len;
-            return null;
-        };
-        
-        const pen: ?u32 = switch (t) {
-            .custom_utf8, .custom_bin => blk: {
-                if (self.data[self.i..].len < 8) {
-                    self.i = self.data.len;
-                    return null;
-                }
-
-                const x = self.data[4..8];
-                break :blk @as(u16, @intCast(x[3])) << 24 | @as(u16, @intCast(x[2])) << 16 | @as(u16, @intCast(x[1])) << 8 | @as(u16, @intCast(x[0]));
-            },
-            else => null,
-        };
-
-        const length = @as(usize, @intCast(self.data[2..4]));
-        const offset = if (pen == null) 4 else 8;
-        var next = offset + length;
-        const x_ = length % 4;
-        if (x_ != 0) {
-
-        }
-    }
-};
+//pub const OptionIterator = struct {
+//    data: []const u8,
+//    i: usize = 0,
+//
+//    pub fn next(self: *@This()) ?Option {
+//        if (self.i >= self.data.len) return null;
+//        if (self.data[self.i..].len < 4) {
+//            self.i = self.data.len;
+//            return null;
+//        }
+//        const t = OptionType.fromRaw(self.data[0..2].*) catch {
+//            self.i = self.data.len;
+//            return null;
+//        };
+//
+//        const pen: ?u32 = switch (t) {
+//            .custom_utf8, .custom_bin => blk: {
+//                if (self.data[self.i..].len < 8) {
+//                    self.i = self.data.len;
+//                    return null;
+//                }
+//
+//                const x = self.data[4..8];
+//                break :blk @as(u16, @intCast(x[3])) << 24 | @as(u16, @intCast(x[2])) << 16 | @as(u16, @intCast(x[1])) << 8 | @as(u16, @intCast(x[0]));
+//            },
+//            else => null,
+//        };
+//
+//        const length = @as(usize, @intCast(self.data[2..4]));
+//        const offset = if (pen == null) 4 else 8;
+//        var next = offset + length;
+//        const x_ = length % 4;
+//        if (x_ != 0) {
+//
+//        }
+//    }
+//};
