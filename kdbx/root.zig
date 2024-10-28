d43dd6a196d4cdce28ff0f0f6665851fbd05f798
David P. Sugar <david@thesugar.de>
2024-10-28 23:42:00 +0100

    kdbx4 body decryption with aes256-cbc and gzip implemented. Bodys larger one block have not been tested yet!

--- kdbx/root.zig
+++ kdbx/root.zig
@@ -267,12 +267,112 @@ pub const Keys = struct {
 // ####################################################
 
 pub const Body = struct {
-    body: []const u8,
+    body: []u8,
     allocator: Allocator,
 
-    pub fn readAlloc(reader: anytype, allocator: Allocator) !@This() {
-        _ = reader;
-        _ = allocator;
+    pub fn readAlloc(
+        reader: anytype,
+        header: *const Header,
+        keys: *const Keys,
+        allocator: Allocator,
+    ) !@This() {
+        var inner = std.ArrayList(u8).init(allocator);
+        errdefer inner.deinit();
+
+        var i: u64 = 0;
+        while (true) : (i += 1) {
+            var mac: [32]u8 = .{0} ** 32;
+            _ = reader.readAll(&mac) catch |e| {
+                std.log.err("unable to read mac of block {d}", .{i});
+                return e;
+            };
+
+            const len = reader.readInt(u32, .little) catch |e| {
+                std.log.err("unable to read length of block {d}", .{i});
+                return e;
+            };
+
+            const curr = inner.items.len;
+
+            // TODO: make this more efficient
+            for (0..len) |_| {
+                try inner.append(try reader.readByte());
+            }
+
+            var raw_block_index: [8]u8 = undefined;
+            std.mem.writeInt(u64, &raw_block_index, i, .little);
+            var raw_block_len: [4]u8 = undefined;
+            std.mem.writeInt(u32, &raw_block_len, len, .little);
+
+            keys.checkMac(&mac, &.{ &raw_block_index, &raw_block_len, inner.items[curr..] }, i) catch |e| {
+                std.log.err("unable to verify authenticity of block {d}", .{i});
+                return e;
+            };
+
+            // Break if length is less than 1MiB
+            if (len < 1048576) break;
+        }
+
+        const iv = header.getEncryptionIv();
+        switch (header.getCipherId()) {
+            .aes128_cbc, .twofish_cbc, .chacha20 => {
+                return error.UnsupportedCipher;
+            },
+            .aes256_cbc => {
+                var xor_vector: [16]u8 = undefined;
+                var j: usize = 0;
+
+                @memcpy(&xor_vector, iv[0..16]);
+                var ctx = std.crypto.core.aes.Aes256.initDec(keys.ekey);
+
+                while (j < inner.items.len) : (j += 16) {
+                    var data: [16]u8 = .{0} ** 16;
+                    const offset = if (j + 16 <= inner.items.len) 16 else inner.items.len - j;
+                    var in_: [16]u8 = undefined;
+                    @memcpy(in_[0..offset], inner.items[j .. j + offset]);
+
+                    ctx.decrypt(data[0..], &in_);
+                    for (&data, xor_vector) |*b1, b2| {
+                        b1.* ^= b2;
+                    }
+
+                    // This could be bad if a block is not divisible by 16 but
+                    // this will only happen for the last block, i.e.,
+                    // doesn't affect the CBC decryption.
+                    @memcpy(&xor_vector, inner.items[j .. j + offset]);
+
+                    @memcpy(inner.items[j .. j + offset], data[0..]);
+                }
+            },
+        }
+
+        switch (header.getCompression()) {
+            .none => {},
+            .gzip => {
+                var in_stream = std.io.fixedBufferStream(inner.items);
+
+                var decompressed = std.ArrayList(u8).init(allocator);
+                errdefer decompressed.deinit();
+
+                try std.compress.gzip.decompress(
+                    in_stream.reader(),
+                    decompressed.writer(),
+                );
+
+                inner.deinit();
+                inner = decompressed;
+            },
+        }
+
+        return @This(){
+            .body = try inner.toOwnedSlice(),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        std.crypto.utils.secureZero(u8, self.body);
+        self.allocator.free(self.body);
     }
 };
 
@@ -844,4 +944,21 @@ test "verify kdbx4 header mac (negative test)" {
     defer keys.deinit();
 
     try std.testing.expectError(error.Authenticity, header.checkMac(&keys));
+}
+
+test "the decryption of a kdbx4 file" {
+    var fbs = std.io.fixedBufferStream(db);
+    const reader = fbs.reader();
+
+    const header = try Header.readAlloc(reader, std.testing.allocator);
+    defer header.deinit();
+
+    var keys = try header.deriveKeys("supersecret", null, null);
+    defer keys.deinit();
+    try header.checkMac(&keys);
+
+    var body = try Body.readAlloc(reader, &header, &keys, std.testing.allocator);
+    defer body.deinit();
+
+    std.debug.print("{s}\n", .{body.body});
 }
